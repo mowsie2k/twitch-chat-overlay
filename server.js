30efb98655fea6c88c4aca1f99fdd75e27a1bf51
mowsie2k <mowsie2k@protonmail.com>
2024-01-26 12:04:33 -0500

    Update port to use environment variable

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
